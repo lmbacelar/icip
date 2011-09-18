9cb4765df59220a37f237c2f6329e5df03ef5d75
Luis Bacelar <lmbacelar@gmail.com>
2011-09-18 12:47:31 +0100

    Added Images to Zone.

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -2,11 +2,13 @@ class Zone < ActiveRecord::Base
 
   CsvColumns = %w[name description inspection_interval]
 
-  attr_accessible :name, :description, :inspection_interval, :items_attributes
+  attr_accessible :name, :description, :inspection_interval, :image_attributes, :items_attributes
 
   belongs_to :configuration
   has_many  :items, :dependent => :destroy
+  has_many  :images, :as => :imageable, :dependent => :destroy
   accepts_nested_attributes_for :items, :reject_if => lambda { |i| i[:name].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :images, :reject_if => lambda { |i| i[:file].blank? }, :allow_destroy => true
 
   validates :name, :presence => true, :uniqueness => {:scope => :configuration_id}
 
