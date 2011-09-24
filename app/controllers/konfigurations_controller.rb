9a5afd22197e6da0a85285648a1066920a479bf2
Luis Bacelar <lmbacelar@gmail.com>
2011-09-24 19:22:08 +0100

    Refactor duplication ou of konfigurations_controller.

--- app/controllers/konfigurations_controller.rb
+++ app/controllers/konfigurations_controller.rb
@@ -1,18 +1,16 @@
 class KonfigurationsController < ApplicationController
+  before_filter :load_resources_from_aircraft, :only => [:new, :create]
+  before_filter :load_resources_from_konfiguration, :except => [:new, :create]
 
   def show
-    @konfiguration = Konfiguration.find(params[:id])
-    @aircraft = @konfiguration.aircraft
   end
 
   def new
-    @aircraft = Aircraft.find(params[:aircraft_id])
     @konfiguration = @aircraft.konfigurations.build
     @konfiguration.number = 1 + ( @aircraft.konfigurations.maximum(:number) || 0 )
   end
 
   def create
-    @aircraft = Aircraft.find(params[:aircraft_id])
     @konfiguration = @aircraft.konfigurations.build(params[:konfiguration])
     if @konfiguration.save
       redirect_to @aircraft, :notice  => "Successfully updated configuration."
@@ -22,13 +20,9 @@ class KonfigurationsController < ApplicationController
   end
 
   def edit
-    @konfiguration = Konfiguration.find(params[:id])
-    @aircraft = @konfiguration.aircraft
   end
 
   def update
-    @konfiguration = Konfiguration.find(params[:id])
-    @aircraft = @konfiguration.aircraft
     if @konfiguration.update_attributes(params[:konfiguration])
       redirect_to @aircraft, :notice  => "Successfully updated configuration."
     else
@@ -37,9 +31,17 @@ class KonfigurationsController < ApplicationController
   end
 
   def destroy
-    @konfiguration = Konfiguration.find(params[:id])
-    @aircraft = @konfiguration.aircraft
     @konfiguration.destroy
     redirect_to @aircraft, :notice => "Successfully destroyed configuration."
+  end
+
+private
+  def load_resources_from_aircraft
+    @aircraft = Aircraft.find(params[:aircraft_id])
+  end
+
+  def load_resources_from_konfiguration
+    @konfiguration = Konfiguration.find(params[:id])
+    @aircraft = @konfiguration.aircraft
   end
 end
