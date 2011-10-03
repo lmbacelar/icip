4f0e55a104fd550fbeb1eaf988d01270b30a9e0b
Luis Bacelar <lmbacelar@gmail.com>
2011-10-03 12:58:28 +0100

    Corrected message output when importing seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,18 @@ puts 'Start seeding ...'
 # Loading Aircrafts
 puts 'Importing Aircrafts from csv ...'
 Aircraft.from_csv 'app/assets/seeds/aircrafts.csv'
-# Loading Configurations / Zones
+# Loading Configurations / Zones / Images
 Aircraft.all.each do |a|
   puts "Importing configurations from csv for #{a.registration} ..."
   a.association_from_csv :konfigurations, 'app/assets/seeds/configurations.csv'
   a.konfigurations.each do |k|
     puts "Importing zones from csv for #{a.registration}, configuration #{k.number} ..."
     k.association_from_csv :zones, 'app/assets/seeds/zones.csv'
+    k.zones.each do |z|
+      puts "Importing images from csv for #{a.registration}, configuration #{k.number}, zone #{z.name} ..."
+      z.images_from_csv "app/assets/seeds/A330_TOE-TOG_#{z.name}.csv"
+    end
   end
 end
-#Loading Images on Zones
-Zone.all.each do |z|
-  puts "Importing images from csv for #{z.konfiguration.aircraft.registration}, configuration #{z.konfiguration}, zone #{z.name} ..."
-  z.images_from_csv "app/assets/seeds/A330_TOE-TOG_#{z.name}.csv"
-end
+
 puts 'Done seeding ...'
