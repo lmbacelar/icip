e9b2917c28d5f2c824afa47d8f150c73c636bb94
Luis Bacelar <lmbacelar@gmail.com>
2011-11-17 19:12:18 +0000

    Added scope and views for assigned inspections. Changed remaining scopes accordingly.

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -3,11 +3,11 @@ class Inspection < ActiveRecord::Base
   has_many :tasks, :dependent => :destroy
   accepts_nested_attributes_for :tasks, :reject_if => lambda { |t| t[:action].blank? ||
                                                                    t[:technician] }, :allow_destroy => true
-  scope :scheduled, where('inspections.execution_date IS NULL')
+  scope :scheduled, where('inspections.assigned_to IS NULL')
+  scope :assigned, where('inspections.assigned_to IS NOT NULL AND inspections.execution_date IS NULL')
   scope :executed, where('inspections.execution_date IS NOT NULL')
   scope :pending, executed.joins(:tasks).merge(Task.pending)
   scope :clean, executed.where('id NOT IN (SELECT inspection_id FROM tasks)')
-  # Closed inspections are those with no tasks added to those with all tasks closed.
   scope :closed, clean + executed.joins(:tasks).merge(Task.closed)
 
   def executed
