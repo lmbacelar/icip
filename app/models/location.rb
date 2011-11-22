7740c049275ca2a632987cee7f7b66da0b3a7123
Luis Bacelar <lmbacelar@gmail.com>
2011-11-22 19:23:40 +0000

    Added validations and sort_natural scope.

--- app/models/location.rb
+++ app/models/location.rb
@@ -8,11 +8,14 @@ class Location < ActiveRecord::Base
   # has_one :image_assignment, :as => :imageable, :dependent => :destroy
   # has_one :image, :through => :image_assignment, :dependent => :destroy
 
+  validates :name, :presence => true
   validates :image, :presence => true
   validates :x1, :inclusion => { :in => 0..1.0/0, :message => 'should be positive' }
   validates :y1, :inclusion => { :in => 0..1.0/0, :message => 'should be positive' }
   validates :x2, :inclusion => { :in => 0..1.0/0, :message => 'should be positive' }
   validates :y2, :inclusion => { :in => 0..1.0/0, :message => 'should be positive' }
+
+  scope :sort_natural, order("LPAD(SUBSTRING(name from '[0-9]+'),5, '0'), SUBSTRING(name from '[^0-9]+')")
 
   def image_id
     image.try(:id)
