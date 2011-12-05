0fc623e766b0fd0edb576479175ef4a26eb3eccf
Luis Bacelar <lmbacelar@gmail.com>
2011-12-05 20:55:39 +0000

    Added image_location virtual attribute. Added image association.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,13 @@ class Item < ActiveRecord::Base
   include CsvSerialize::InstanceMethods
   CsvColumns = %w[name part.number part.kind part.description]
 
-  attr_accessible :name, :kind, :part_number, :part_id, :location
+  attr_accessible :name, :kind, :part_number, :part_id, :image_location
 
   belongs_to :zone
   belongs_to :part
   has_one :location_assignment, :as => :locatable, :dependent => :destroy
   has_one :location, :through => :location_assignment
-  # has_one :location, :as => :locatable, :dependent => :destroy
+  has_one :image, :through => :location
 
   validates :name, :presence => true, :uniqueness => { :scope => :zone_id }
   validates :part, :presence => true
@@ -30,5 +30,19 @@ class Item < ActiveRecord::Base
   end
   def part_number=(number)
     self.part = Part.find_by_number(number) if number.present?
+  end
+
+  def image_location
+    "#{location.name}, on image ##{location.image_id} (#{File.basename(location.image.file_url)})" if location
+  end
+  def image_location=(args)
+    unless args.nil?
+      args=args.split(',')
+      if args.size > 1
+        self.location = Location.find_by_image_id_and_name(args[1][/(?<=#)[\d]*/], args[0])
+      else
+        self.location = nil
+      end
+    end
   end
 end
