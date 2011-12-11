93666bca748155161db5737fbdd72c6b6f7565b8
lmbacelar <lmbacelar@gmail.com>
2011-12-11 21:12:23 +0000

    Seeding admin, engineer and technician Users and Roles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,26 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 puts 'START Seeding ...'
+puts 'CREATING Roles ...'
+[:admin, :engineer, :technician].each do |r|
+  Role.create :name => r
+end
+puts 'CREATING Users ...'
+User.create(:email => 'admin@icip.com',
+            :tap_number => '000000',
+            :name => 'Administrator',
+            :password => 'admin',
+            :password_confirmation => 'admin').roles << Role.find_by_name(:admin)
+User.create(:email => 'engineer@icip.com',
+            :tap_number => '000001',
+            :name => 'Engineer',
+            :password => 'engineer',
+            :password_confirmation => 'engineer').roles << Role.find_by_name(:engineer)
+User.create(:email => 'technician@icip.com',
+            :tap_number => '000002',
+            :name => 'Technician',
+            :password => 'technician',
+            :password_confirmation => 'technician').roles << Role.find_by_name(:technician)
 # Loading Aircrafts
 puts 'IMPORTING Aircrafts from csv ...'
 Aircraft.from_csv 'db/seeds/aircrafts.csv'
