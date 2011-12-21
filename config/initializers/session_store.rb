f5a15e5cc33b7950ab1c2f94154d0e9fea5de0d7
Luis Bacelar <lmbacelar@gmail.com>
2011-12-21 16:24:14 +0000

    Session management done through database.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Icip::Application.config.session_store :cookie_store, key: '_icip_session'
+#Icip::Application.config.session_store :cookie_store, key: '_icip_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Icip::Application.config.session_store :active_record_store
+Icip::Application.config.session_store :active_record_store
