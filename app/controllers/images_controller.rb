58bc615bb4e991ef4f77379972c6e0be53ba76b5
Luis Bacelar <lmbacelar@gmail.com>
2011-12-26 18:19:28 +0000

    Added new image viewn / controller actions.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,23 @@ class ImagesController < AuthorizedController
     @images = Image.all
   end
 
+  def new
+  end
+
+  def create
+    if @image.save
+      redirect_to @image, notice: 'Successfully created image.'
+    else
+      render action: 'new'
+    end
+  end
+
   def show
     @locations = @image.locations
+  end
+
+  def destroy
+    @image.destroy
+    redirect_to images_url, notice: 'Successfully destroyed image.'
   end
 end
