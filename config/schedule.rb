ca93705387b82907abfb8a72dcf2e5bf5b8c56a0
lmbacelar <lmbacelar@gmail.com>
2011-12-30 11:20:56 +0000

    Tire index rebuild on Inspection after scheduling of inspections.

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,7 @@ set :output, "#{path}/log/cron.log"
 # Schedule inspections
 every 12.hours do
   runner "Zone.order('zones.id DESC').schedule_inspections(1)"
+  rake "environment tire:import CLASS='Inspection' FORCE=true"
 end
 
 # Reindex Parts and Inspections
