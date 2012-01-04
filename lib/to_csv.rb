5cdbd3a85224bafb670d5296ed3a6bc11497c3ed
Luis Bacelar <lmbacelar@gmail.com>
2012-01-04 20:13:06 +0000

    Added to_csv method to Array, ActiveRecord::Base. This allows use of format.csv on controllers, for ActiveRecord objects and collections.

---
+++ lib/to_csv.rb
@@ -0,0 +1,36 @@
+class Array
+  def to_csv(options = {}, csv_options = {})
+    return '' if self.empty?
+
+    klass      = self.first.class
+    attributes = self.first.attributes.keys.sort.map(&:to_sym)
+
+    if options[:only]
+      columns = Array(options[:only]) & attributes
+    else
+      columns = attributes - Array(options[:except])
+    end
+
+    columns += Array(options[:methods])
+
+    return '' if columns.empty?
+
+    writer = RUBY_VERSION >= "1.9.0" ? CSV : FasterCSV
+
+    output = writer.generate(csv_options) do |csv|
+      csv << columns.map { |column| klass.human_attribute_name(column) } unless options[:headers] == false
+      self.each do |item|
+        csv << columns.collect { |column| item.send(column) }
+      end
+    end
+
+    output
+  end
+
+end
+
+class ActiveRecord::Base
+  def to_csv(options = {}, csv_options = {})
+    [self].to_csv(options, csv_options)
+  end
+end
