858b19e20510dc264ec9590c0961c8894d12aefc
lmbacelar <lmbacelar@gmail.com>
2012-01-05 23:35:15 +0000

    Added links_to_export helper. Renders links to accepted format for the current action, other than html.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def icon_to(text, url, args = {})
     link_to(nil, url, args.merge(class: text.downcase, title: text.capitalize, alt: text.capitalize))
   end
+
+  def links_to_export(args = {})
+    mimes = controller.mimes_for_action(action_name)
+    output = []
+    mimes.delete(:html)
+    mimes.each { |k,v| output << link_to(k.to_s.upcase, params.merge(format: k)) }
+    content_tag :div, output.join(args[:separator]).html_safe, class: (args[:class] || 'links_to_export')
+  end
 end
