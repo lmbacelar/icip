227dd3a3ff508e5713c0c5b6e0d5d266aa2035bf
lmbacelar <lmbacelar@gmail.com>
2012-04-01 14:33:34 +0100

    Specify role of tasks to :app server only.

--- config/recipes/uploads.rb
+++ config/recipes/uploads.rb
@@ -3,7 +3,7 @@ namespace :uploads do
     Creates the upload folders unless they exist
     and sets the proper upload permissions.
   EOD
-  task :setup, :except => { :no_release => true } do
+  task :setup, roles: :app, except: { no_release: true } do
     dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
     run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
   end
@@ -11,7 +11,7 @@ namespace :uploads do
     [internal] Creates the symlink to uploads shared folder
     for the most recently deployed version.
   EOD
-  task :symlink, :except => { :no_release => true } do
+  task :symlink, roles: :app, except: { no_release: true } do
     run "rm -rf #{release_path}/public/uploads"
     run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
   end
@@ -19,7 +19,7 @@ namespace :uploads do
     [internal] Computes uploads directory paths
     and registers them in Capistrano environment.
   EOD
-  task :register_dirs do
+  task :register_dirs, roles: :app do
     set :uploads_dirs,    %w(uploads)
     set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
   end
