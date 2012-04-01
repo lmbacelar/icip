570c34a74ff159578563c0526e6274ddd97731b9
lmbacelar <lmbacelar@gmail.com>
2012-04-01 14:25:53 +0100

    Clarify pre-requisites message.

--- config/recipes/base.rb
+++ config/recipes/base.rb
@@ -14,16 +14,18 @@ end
 namespace :deploy do
   desc "Install everything onto the server"
   task :install, roles: :app do
-    puts "\nPRE_REQUISITES:\n-#{user} user setup, with sudo access.\nOn Arch do:\n"
+    puts "\nPRE_REQUISITES:\n\n"
+    puts "\ta. '#{user}' user created\n"
+    puts "\tb. '#{user}' with sudo access\n"
+    puts "\tc. Updated system\n"
+    puts "\n\tOn Arch do:\n"
     puts "\t1. Login via ssh on root user.\n"
     puts "\t2. > adduser deployer\n\t\t(primary group users, secondary group wheel, accept defaults and set password)\n"
     puts "\t3. > visudo\n\t\t(uncomment line '%wheel ALL=(ALL) ALL')\n"
     puts "\t4. > pacman -Syy\n\t\t(synchronize database)\n"
-    puts "\t5. > pacman -Syu\n\t\t(update system)\n"
-    puts "\t        NOTE: you might need to run this twice if pacman needs upgrading.\n"
+    puts "\t5. > pacman -Syu\n\t\t(update system)\n\t\tNOTE: you might need to run this twice if pacman needs upgrading.\n"
     puts "\n\n\n\t\t\tIMPORTANT !!!\n\n"
     puts "\tDo not forget to manually add daemons that require autostart to rc.conf DAEMONS section.\n\n\n"
-    puts "\tAll Done..."
     run "#{sudo} pacman -Sq --noconfirm base-devel"
   end
 end
